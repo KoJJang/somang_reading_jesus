89a41f5260a29a2ce2c14fbeae6883650d68e185
KoJJang <yuiyui6258@gmail.com>
2025-02-26 22:49:28 +0900

    firebase 연동 제거 후 정상 동작 확인

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_core/firebase_core.dart';
 import 'package:reading_jesus_somang/features/home/screens/home_screen.dart';
 import 'package:reading_jesus_somang/core/constants/theme.dart';
 import 'package:reading_jesus_somang/features/layout/app_layout.dart';
 import 'package:reading_jesus_somang/data/services/database_service.dart';
 import 'package:reading_jesus_somang/features/bible/screens/bible_reading_screen.dart';
 import 'package:reading_jesus_somang/features/calendar/screens/calendar_screen.dart';
-import 'firebase_options.dart';
+// import 'firebase_options.dart';
 
 void main() async {
   // Flutter 엔진과 위젯 바인딩 초기화
   WidgetsFlutterBinding.ensureInitialized();
 
   // Firebase 초기화
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   // 데이터베이스 서비스 초기화
   final dbService = DatabaseService();
