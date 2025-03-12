d05b0f75d0e077fa817729f8c29f3b517a3708ce
KoJJang <yuiyui6258@gmail.com>
2025-03-13 00:16:41 +0900

    daily_plan 카드 수정

--- lib/features/home/widgets/daily_plan.dart
+++ lib/features/home/widgets/daily_plan.dart
@@ -21,7 +21,7 @@ class DailyPlan extends StatelessWidget {
               borderRadius: BorderRadius.circular(12),
             ),
             child: const Icon(
-              Icons.coffee_outlined,
+              Icons.church_outlined,
               color: Color(0xFF4F46E5),
               size: 24,
             ),
@@ -32,7 +32,7 @@ class DailyPlan extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 const Text(
-                  '오늘의 말씀',
+                  '리딩 지저스',
                   style: TextStyle(
                     fontSize: 18,
                     fontWeight: FontWeight.w500,
@@ -41,32 +41,14 @@ class DailyPlan extends StatelessWidget {
                 ),
                 const SizedBox(height: 4),
                 const Text(
-                  '매일 새로운 구절을 발견해보세요',
+                  '그리스도 중심으로 성경 읽기',
+                  style: TextStyle(fontSize: 14, color: Color(0xFF4B5563)),
+                ),
+                const Text(
+                  '함께 말씀 통독을 시작해보세요!',
                   style: TextStyle(fontSize: 14, color: Color(0xFF4B5563)),
                 ),
                 const SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    // backgroundColor: const Color(0xFF4F46E5),
-                    backgroundColor: Colors.grey,
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 16,
-                      vertical: 8,
-                    ),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                  ),
-                  child: const Text(
-                    '아직 지원하지 않습니다',
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w500,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
               ],
             ),
           ),
